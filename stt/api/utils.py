c0b24116d2e96f3d170acd783dda5bd6ec5f0650
jissagn <7462969+jissagn@users.noreply.github.com>
2023-06-30 14:53:33 +0200

    Fix return model as FastAPI/starlett put all in a 'detail' key

--- stt/api/utils.py
+++ stt/api/utils.py
@@ -1,24 +1,48 @@
 from typing import Any, Sequence
 
+from pydantic import BaseModel
+
 from stt.exceptions import ExceptionModel, RichHTTPException
 
 
+class ReturnedExceptionModel(BaseModel):
+    """
+    Class to wrap what we want to output, as
+    FastAPI puts everything into a `detail` field.
+    """
+
+    detail: ExceptionModel
+
+
 def error_responses_for_openapi(
-    exceptions: Sequence[RichHTTPException] | None = None,
-) -> dict[int | str, dict[str, Any]]:
+    excs: Sequence[RichHTTPException] | None = None,
+) -> dict[
+    int | str, dict[str, Any]
+]:  # "int | str" here is to make Pylance happy, but it really has int keys only
+    """
+        Helper to declare what JSON we return depending on errors status code.
+        It's intended to be passed to the `responses` parameter of FastAPI routes.
+        FastAPI will use this to generate proper OpenAPI documentation.
+
+    Args:
+        excs (Sequence[RichHTTPException] | None, optional): The exceptions that could be raised. Defaults to None.
+
+    Returns:
+        dict[int | str, dict[str, Any]]: The expected declaration of returned models.
+    """
     r = (
         {
             exc.status_code: {
-                "model": ExceptionModel,
+                "model": ReturnedExceptionModel,
                 "description": exc.description,
             }
-            for exc in exceptions
+            for exc in excs
         }
-        if exceptions
+        if excs
         else {}
     )
     r[500] = {
-        "model": ExceptionModel,
+        "model": ReturnedExceptionModel,
         "description": "Internal error",
     }
     return r  # type: ignore
